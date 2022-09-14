5e3f5288075f288b6574f0df1d4a583ec2f32f37
Bair Arshaev <ars-bair@yandex.ru>
2022-09-14 23:33:45 +0300

    DI with setter

--- src/main/java/ru/arshaev/spring/spring_introduction/Person.java
+++ src/main/java/ru/arshaev/spring/spring_introduction/Person.java
@@ -4,8 +4,17 @@ public class Person {
 
     private Pet pet;
 
-    public Person(Pet pet) {
+//    public Person(Pet pet) {
+//        System.out.println("Person Bean is created");
+//        this.pet = pet;
+//    }
+
+    public Person() {
         System.out.println("Person Bean is created");
+    }
+
+    public void setPet(Pet pet) {
+        System.out.println("Class Person: set Pet");
         this.pet = pet;
     }
 
